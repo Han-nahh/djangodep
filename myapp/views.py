88faa60a8efbc98ab2085aa65abe8589030e8c9c
Han-nahh <100560594+Han-nahh@users.noreply.github.com>
2023-07-13 10:07:37 +0300

    first dep

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, World!")
+
+def about(request):
+    return HttpResponse("About page")
+
+def contact(request):
+    return HttpResponse("Contact page")
